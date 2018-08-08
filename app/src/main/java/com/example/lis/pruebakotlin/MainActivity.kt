3debceed4aa201bdbe6a9bde4d2f1366d6c1a8bd
santiago-b9826 <santiago.b9826@gmail.com>
2018-08-08 15:44:52 -0500

    Avances

--- app/src/main/java/com/example/lis/pruebakotlin/MainActivity.kt
+++ app/src/main/java/com/example/lis/pruebakotlin/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.lis.pruebakotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+
+    private var editName: EditText? = null
+    private var editAge: EditText? = null
+    private var button: Button? = null
+    private var name: String? = null
+    private var age = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editName = findViewById(R.id.editText_Name)
+        editAge = findViewById(R.id.editText_Age)
+        button = findViewById(R.id.button)
+
+        editName?.addTextChangedListener(this)
+        //editAge?.addTextChangedListener(this)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Toast.makeText(this, "Hola " + p0.toString(), Toast.LENGTH_SHORT).show()
     }
 }
